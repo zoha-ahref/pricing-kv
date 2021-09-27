105e1a176680b44b7e9398c4ae6f754ccbf03f23
zoha <sweetangelzoha@gmail.com>
2021-09-26 21:40:45 -0400

    Commit all files

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+
+// document.getElementById('#angle-btn').addEventListener('click', fav);
+
+// function fav() {
+//     // var element = document.getElementById("ic");
+//     // element.classList.toggle("fa-solid fa-angle-up");
+//     document.parent().find(".fa-angle-down").removeClass("fa-angle-down").addClass("fa-angle-up");
+
+//  };
+
+ function myFunction(x) {
+    x.classList.toggle("fa-angle-up");
+
+ }
+
